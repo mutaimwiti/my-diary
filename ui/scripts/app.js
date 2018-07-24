20130e3d0c7e26de9767a495ea27be33fb8b0117
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-07-24 14:22:16 +0300

    Add app landing page

--- ui/scripts/app.js
+++ ui/scripts/app.js
@@ -2,14 +2,10 @@ document.onreadystatechange = function () {
     if (document.readyState == "interactive") {
         const href = window.location.href;
         const uri = href.substr(href.lastIndexOf('/') + 1);
-        if (uri === '') {
-            console.log(document.querySelector("a[href='index.html']"));
-            document.querySelector("a[href='index.html']").className += ' active';
-        } else {
+
             const current = document.querySelector("a[href='" + uri + "']");
             if (current !== null) {
                 current.className += ' active';
             }
-        }
     }
 };
